df6af1f5cd182eae8d3e3858a4ad9665d006ca33
Tafsirul Islam Shafin <107920202+0SHAFIN@users.noreply.github.com>
2025-01-26 00:29:09 +0600

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,130 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck ,faArrowRight,faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { on } from "events";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [imageSrc, setImageSrc] = useState("/icon/icon_bw.png"); // Default image source
+  const [hoverActivated, setHoverActivated] = useState(false); // Default hover state
+ 
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div>
+      <div className="slide h-screen bg-[var(--first-slide-bg)] text-[var(--first-slide-text)] px-28">
+        <div className="nav flex justify-between items-center p-4">
+          <p className="text-xl font-bold text-[var(--first-slide-text)]">PoopUp</p>
+          <div className="nav-links">
+            <a className="px-5" href="#">Pricing</a>
+            <a className="px-5" href="#">FAQ</a>
+          </div>
+          <button className="bg-[var(--button-bg)] text-[var(--button-text)] text-sm p-3 px-5 rounded-3xl font-bold hover:shadow-lg transition-transform duration-300 hover:bg-[#FFA629]"
+          >Get Started</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+         
+         {/* Title part */}
+
+        <div className="display flex flex-col justify-center relative mt-24">
+          <span>
+            {/* Text Content */}
+            <p className="text-5xl font-extrabold text-center mt-10"
+              onMouseEnter={() => {
+                setImageSrc("/icon/icon_color.png");
+                setHoverActivated(true);
+              }}
+              onMouseLeave={() => {
+                setImageSrc("/icon/icon_bw.png");
+                setHoverActivated(false);
+              }}
+            >
+              Turn Visitors Into Customers
+              <br />
+              With wake-up call popups
+            </p>
+
+            {/* Positioned Icon with Hover Behavior */}
+            <div
+              className="">
+              <Image
+                src={imageSrc} // Dynamically update the image source
+                width={70}
+                height={70}
+                className={`absolute top-9 left-3/4 transform -translate-x-20 -translate-y-1/2 ${hoverActivated ? "scale-110 transition-transform duration-300 rotate-12 shadow-lg rounded-full" : ""}`}
+                alt="Money Emoji"
+                onMouseEnter={() => {
+                  setImageSrc("/icon/icon_color.png");
+                  setHoverActivated(true);
+                }}
+                onMouseLeave={() => {
+                  setImageSrc("/icon/icon_bw.png");
+                  setHoverActivated(false);
+                }}
+              />
+            </div>
+
+            <div className="flex justify-center mt-4">
+              <p className={`text-5xl font-extrabold text-[var(--button-bg)] ${hoverActivated ? "text-green-800" : ""}`}
+                onMouseEnter={() => {
+                  setImageSrc("/icon/icon_color.png");
+                  setHoverActivated(true);
+                }}
+                onMouseLeave={() => {
+                  setImageSrc("/icon/icon_bw.png");
+                  setHoverActivated(false);
+                }}
+              >
+                - - - - - - - - - - - - -
+              </p>
+            </div>
+
+            <div className="flex justify-center mt-4">
+              <p className="text-xl text-center">
+                Delivers impactful and attention-grabbing pop-up notifications <br></br> to confront website visitors with the harsh realities, driving <br></br> engagement and conversions.
+              </p>
+            </div>
+
+            <div className="flex justify-center mt-8">
+              <FontAwesomeIcon icon={faCheck} className="text-xl text-green-600 mx-2"/>
+              <p className="text-center">Pay once, user forever</p>
+            </div>
+            <div className="flex justify-center mt-3">
+              <FontAwesomeIcon icon={faCheck} className="text-xl text-green-600 mx-2"/>
+              <p className="text-center">1-minute no-code setup</p>
+            </div>
+            <div className="flex justify-center mt-3">
+              <FontAwesomeIcon icon={faCheck} className="text-xl text-green-600 mx-2"/>
+              <p className="text-center">Increase conversion rate</p>
+            </div>
+
+            {/* get poopup button */}
+            <div className="flex justify-center">
+              <button className="px-20 py-4 bg-[var(--button-bg)] text-[var(--button-text)] text-sm p-3 rounded-3xl font-bold hover:shadow-lg transition-transform duration-300 hover:bg-[#FFA629] mt-10">
+                Get PoopUp <FontAwesomeIcon icon={faArrowRight} className="ml-2"/>
+              </button>
+            </div>
+            <p className="text-center mt-10">
+              <FontAwesomeIcon icon={faAngleUp} className="text-xl text-gray-500 mx-2"/>
+            </p>
+            <div className="flex justify-center">
+              <div className="flex w-fit rounded-full bg-[var(--card-light-bg)] ">
+                  <Image src="/icon/person.jpg" width={80} height={80} className="rounded-full" alt="Person Image"/>
+                  <div>
+                    <div className="flex mr-20 mt-3 mb-1 mx-5">
+                      <p className="px-4 text-lg">This is so clever</p>
+                      <Image className="" src="/icon/clap.png" width={20} height={20} alt="Clap Icon"/>
+                    </div>
+                    <p className="px-7 text-sm">Guillermo R. -Vercel CEO</p>
+                  </div>
+              </div>
+            </div>
+
+          </span>
+        </div>
+      </div>
+      <div className="slide h-screen bg-blue-500">
+        <h1>Second Slide</h1>
+      </div>
     </div>
   );
 }
