6ab91401e4e1381651d7ef13d0d4d71a482f0143
Tanvir <tanvirahmedabd1@gmail.com>
2025-02-03 17:57:52 +0600

    refactor: improve SortableItem component and enhance Manage page functionality

--- app/dashboard/manage/SortableItem.tsx
+++ app/dashboard/manage/SortableItem.tsx
@@ -15,53 +15,28 @@ export default function SortableItem({ id, name, image, content, timeAgo }: Sort
     const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
 
     return (
-        <div
-            ref={setNodeRef}
-            {...attributes}
-            className="p-4 bg-[--first-slide-bg] rounded-xl shadow-md flex items-center"
-            style={{
-            transform: CSS.Transform.toString(transform),
-            transition,
-            }}
-        >
-            <div
-            {...listeners}
-            className="mr-8 grid grid-cols-2 gap-x-2 gap-y-1 cursor-grab active:cursor-grabbing"
-            >
-            <div className="w-1 h-1 bg-[var(--first-slide-text)] rounded-full"></div>
-            <div className="w-1 h-1 bg-[var(--first-slide-text)] rounded-full"></div>
-            <div className="w-1 h-1 bg-[var(--first-slide-text)] rounded-full"></div>
-            <div className="w-1 h-1 bg-[var(--first-slide-text)] rounded-full"></div>
-            <div className="w-1 h-1 bg-[var(--first-slide-text)] rounded-full"></div>
-            <div className="w-1 h-1 bg-[var(--first-slide-text)] rounded-full"></div>
+        <div ref={setNodeRef} {...attributes} className="p-4 bg-[--first-slide-bg] rounded-xl flex items-center" style={{ transform: CSS.Transform.toString(transform), transition, }}>
+            <div {...listeners} className="ml-2 mr-6 grid grid-cols-2 gap-x-2 gap-y-1 cursor-grab active:cursor-grabbing">
+                <div className="w-1 h-1 bg-[var(--button-text)] rounded-full"></div>
+                <div className="w-1 h-1 bg-[var(--button-text)] rounded-full"></div>
+                <div className="w-1 h-1 bg-[var(--button-text)] rounded-full"></div>
+                <div className="w-1 h-1 bg-[var(--button-text)] rounded-full"></div>
+                <div className="w-1 h-1 bg-[var(--button-text)] rounded-full"></div>
+                <div className="w-1 h-1 bg-[var(--button-text)] rounded-full"></div>
             </div>
             <div className="flex-shrink-0 mr-4">
-            <input type="file" className="hidden" id={`upload-${id}`} />
-            <label htmlFor={`upload-${id}`} className="relative">
-            <img src={image} className="w-12 h-12 text-[var(--first-slide-text)] object-cover rounded-lg cursor-pointer text-sm" />
-            {!image && (
-            <i className="fas fa-camera absolute inset-0 flex items-center justify-center text-[var(--first-slide-text)] text-xl"></i>
-            )}
-            </label>
+                <input type="file" className="hidden" id={`upload-${id}`}/>
+                <label htmlFor={`upload-${id}`} className="relative">
+                    <img src={image} className="w-12 h-12 text-[var(--first-slide-text)] object-cover rounded-lg cursor-pointer text-sm" />
+                    {!image && (<i className="fas fa-camera absolute inset-0 flex items-center justify-center text-[var(--first-slide-text)] text-xl"></i>)}
+                </label>
             </div>
             <div className="flex-grow">
-            <input
-            type="text"
-            defaultValue={name}
-            className="w-full bg-[--first-slide-bg] text-[var(--first-slide-text)] p-1 rounded-lg mb-1 text-sm font-bold focus:outline-none"
-            />
-            <input
-            type="text"
-            defaultValue={content}
-            className="w-full bg-[--first-slide-bg] text-[var(--first-slide-text)] p-1 rounded-lg text-sm focus:outline-none"
-            />
+                <input type="text" defaultValue={name} className={`w-full bg-[--first-slide-bg] text-[var(--first-slide-text)] p-1 rounded-lg mb-1 text-sm font-bold focus:outline-none border ${!name ? 'border-gray-300' : 'border-transparent'} focus:border-gray-300`}/>
+                <input type="text" defaultValue={content} className={`w-full bg-[--first-slide-bg] text-[var(--first-slide-text)] p-1 rounded-lg text-sm focus:outline-none border ${!content ? 'border-gray-300' : 'border-transparent'} focus:border-gray-300`}/>
             </div>
-            <div className="ml-4 mb-auto">
-            <input
-            type="text"
-            defaultValue={timeAgo}
-            className="w-12 bg-[--first-slide-bg] text-[var(--first-slide-text)] p-1 rounded-lg text-sm focus:outline-none"
-            />
+            <div className="ml-2 mb-auto">
+                <input type="text" defaultValue={timeAgo} className={`w-10 bg-[--first-slide-bg] text-[var(--first-slide-text)] p-1 rounded-lg text-xs focus:outline-none border ${!timeAgo ? 'border-gray-300' : 'border-transparent'} focus:border-gray-300`}/>
             </div>
         </div>
     );
--- app/dashboard/manage/page.tsx
+++ app/dashboard/manage/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
+import dynamic from 'next/dynamic';
 import { useState, useEffect } from 'react';
 import { auth } from "../../../lib/firebase";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faArrowLeft, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faArrowLeft, faTimes, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { DndContext, closestCenter, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
 import { SortableContext, arrayMove, verticalListSortingStrategy } from "@dnd-kit/sortable";
-import SortableItem from "./SortableItem";
+
+const SortableItem = dynamic(() => import('./SortableItem'), { ssr: false });
 
 const initialItems = [
     {
         id: "1",
-        image: "/icon/poopxd.pn",
+        image: "/icon/poopxd.png",
         title: "Notification 1",
         content: "This is the first notification",
         timeAgo: "5m",
@@ -37,10 +39,13 @@ export default function Manage() {
   const [isTestRunning, setIsTestRunning] = useState(false);
   const [user, setUser] = useState<User | null>(null);
   const [items, setItems] = useState(initialItems);
-
-  // Configure drag sensors (mouse/touch)
+  const [isClient, setIsClient] = useState(false);
   const sensors = useSensors(useSensor(PointerSensor));
-  
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
@@ -49,7 +54,6 @@ export default function Manage() {
     return () => unsubscribe();
   }, []);
 
-  // Handle drag end
   const handleDragEnd = (event: any) => {
     const { active, over } = event;
     if (active.id !== over.id) {
@@ -61,53 +65,56 @@ export default function Manage() {
     }
   };
 
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <div className="min-h-screen bg-[--usecase-card] flex flex-col">
-      {/* Header with Increased Padding */}
       <header className="bg-[--first-slide-bg] py-4 px-[26rem]">
         <div className="flex justify-between items-center">
-
           <button className="bg-[var(--usecase-card)] text-[var(--button-text)] text-sm p-3 px-5 rounded-3xl font-bold hover:shadow-lg transition-transform duration-300 hover:bg-[#FFA629] flex items-center gap-2">
             <FontAwesomeIcon icon={faArrowLeft} className="text-[var(--button-text)]" />
             Back
           </button>
 
-          <button
-            className="bg-[var(--button-bg)] text-[var(--button-text)] text-sm p-3 px-5 rounded-3xl font-bold hover:shadow-lg transition-transform duration-300 hover:bg-[#FFA629] flex items-center gap-2"
-            onClick={() => setIsTestRunning(!isTestRunning)}
-          >
+          <button className="bg-[var(--button-bg)] text-[var(--button-text)] text-sm p-3 px-5 rounded-3xl font-bold hover:shadow-lg transition-transform duration-300 hover:bg-[#FFA629] flex items-center gap-2" onClick={() => setIsTestRunning(!isTestRunning)}>
             <FontAwesomeIcon icon={isTestRunning ? faTimes : faPlay} className="text-[var(--button-text)]" />
             {isTestRunning ? 'Stop' : 'Test'}
           </button>
         </div>
       </header>
+      
       <div className="flex row">
         <div className="flex-grow ml-[26rem] mr-8 mt-16 text-[var(--first-slide-text)] font-extrabold text-xl">
           poopup.co
-          {/* PoopUps */}
-            <div className="flex-grow">
-            </div>
+          <div className="flex-grow" />
         </div>
-        {/* Main Content */}
+        
         <div className="flex-grow-0 w-[30rem] ml-6 mt-32 mr-[26rem] rounded-3xl shadow-sm p-4 min-w-[300px] border border-gray-300">
           <div className="space-y-6">
-          <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-            <SortableContext items={items} strategy={verticalListSortingStrategy}>
+            <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+              <SortableContext items={items} strategy={verticalListSortingStrategy}>
                 <div className="space-y-6 p-4">
-                {items.map((item) => (
-                    <SortableItem 
-                        key={item.id} 
-                        id={item.id} 
-                        name={item.title} 
-                        image={item.image}
-                        content={item.content} 
-                        timeAgo={item.timeAgo} 
-                    />
-                ))}
+                  {items.map((item) => (<SortableItem key={item.id} id={item.id} name={item.title} image={item.image} content={item.content} timeAgo={item.timeAgo}/>))}
                 </div>
-            </SortableContext>
+              </SortableContext>
             </DndContext>
           </div>
+          <button className="bg-[var(--button-text)] text-white text-xs p-2 px-12 rounded-3xl hover:shadow-lg transition-transform duration-300 hover:bg-[var(--first-slide-text)] flex items-center gap-2 m-4 mt-2 ml-auto"
+            onClick={() => {
+              const newItem = {
+                id: (items.length + 1).toString(),
+                image: null,
+                title: null,
+                content: null,
+                timeAgo: null,
+              };
+                setItems([...items, newItem as any]);
+            }}>
+            <FontAwesomeIcon icon={faPlus} className="text-white" />
+            Message
+          </button>
         </div>
       </div>
     </div>
